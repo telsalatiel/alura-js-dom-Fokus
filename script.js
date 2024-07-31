1304a5fc0e561fb9b2b051a7397c247c639a45b0
telsalatiel <tel.salatiel2@gmail.com>
2024-07-31 17:05:37 -0300

    icone do botão alterado

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ const titulo  = document.querySelector('.app__title')
 const botoes = document.querySelectorAll('.app__card-button')
 const startPauseBt = document.querySelector('#start-pause')
 const musicaFocoInput = document.querySelector('#alternar-musica')
+const iniciarOuPausarBt = document.querySelector('#start-pause span')
+const iniciarOuPausarBtIcone = document.querySelector('.app__card-primary-butto-icon')
 const musica = new Audio('/sons/luna-rise-part-one.mp3')
 const audioPlay = new Audio('/sons/play.wav')
 const audioPausa = new Audio('/sons/pause.mp3')
@@ -93,8 +95,12 @@ function iniciarOuPausar() {
   }
   audioPlay.play()
   intervaloId = setInterval(contagemRegressiva, 1000)
+  iniciarOuPausarBtIcone.setAttribute('src', `/imagens/pause.png`)
+  iniciarOuPausarBt.textContent = "Pausar"
 }
 function zerar() {
   clearInterval(intervaloId)
+  iniciarOuPausarBtIcone.setAttribute('src', `/imagens/play_arrow.png`)
+  iniciarOuPausarBt.textContent = "Começar"
   intervaloId = null
 }
