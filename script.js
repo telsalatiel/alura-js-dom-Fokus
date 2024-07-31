098f9580ab69cdd7554e47d44a4340a8b1fbf42b
telsalatiel <tel.salatiel2@gmail.com>
2024-07-31 16:42:23 -0300

    contador e sons acrescentados

--- script.js
+++ script.js
@@ -6,11 +6,18 @@ const timer = document.querySelector('#timer')
 const banner = document.querySelector('.app__image')
 const titulo  = document.querySelector('.app__title')
 const botoes = document.querySelectorAll('.app__card-button')
+const startPauseBt = document.querySelector('#start-pause')
 const musicaFocoInput = document.querySelector('#alternar-musica')
 const musica = new Audio('/sons/luna-rise-part-one.mp3')
+const audioPlay = new Audio('/sons/play.wav')
+const audioPausa = new Audio('/sons/pause.mp3')
+const audioTempoFinalizado = new Audio('/sons/beep.mp3')
 const duracaoFoco = 1500; 
 const duracaoDescansoCurto = 300; 
 const duracaoDescansoLongo = 900; 
+
+let tempoDecorridoEmSegundos = 5
+let intervaloId = null
 
 musica.loop = true
 musicaFocoInput.addEventListener('change', () => {
@@ -63,4 +70,31 @@ function alterarContexto(contexto) {
         `
       break;
   }
+}
+
+const contagemRegressiva = () => {
+  if(tempoDecorridoEmSegundos <= 0){
+    audioTempoFinalizado.play()
+    alert('Tempo finalizado!')
+    zerar()
+    return
+  }
+  tempoDecorridoEmSegundos -= 1
+  console.log('Temporizador' + tempoDecorridoEmSegundos) 
+}
+
+startPauseBt.addEventListener('click', iniciarOuPausar)
+
+function iniciarOuPausar() {
+  if(intervaloId){
+    audioPausa.play()
+    zerar()
+    return
+  }
+  audioPlay.play()
+  intervaloId = setInterval(contagemRegressiva, 1000)
+}
+function zerar() {
+  clearInterval(intervaloId)
+  intervaloId = null
 }
